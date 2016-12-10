b8fd4c3087850eb28e66b5293b27f8b1e26425f8
Tom Gobich <fallofmars@gmail.com>
2016-12-10 16:39:36 -0500

    navigation medium and up complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Logo from '../images/logo.svg';
 
 const Header = (props) => {
 	return (
-		<div className="header navbar navbar-default">
+		<div className="header navbar navbar-default navbar-fixed-top">
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-8 col-md-3">
@@ -15,14 +15,25 @@ const Header = (props) => {
               </Isvg>
             </div>
           </div>
-          <div className="col-xs-4 col-md-6">
-            <nav className="navigation">
-              <a className="home"><li>Home</li></a>
-              <a className="about"><li>About</li></a>
-              <a className="portfolio"><li>Portfolio</li></a>
-              <a className="contact"><li>Contact</li></a>
-              <div className="h-rule"></div>
-            </nav>
+          <div className="col-xs-4 col-md-9">
+            <div className="row">
+              <div className="col-md-9">
+                <nav className="navigation">
+                  <a className="home"><li>Home</li></a>
+                  <a className="about"><li>About</li></a>
+                  <a className="portfolio"><li>Portfolio</li></a>
+                  <a className="contact"><li>Contact</li></a>
+                  <div className="h-rule"></div>
+                </nav>
+              </div>
+              <div className="col-md-3">
+                <nav className="social-networks">
+                  <a className="github"><li><img src="http://placehold.it/30x30" alt="GitHub" /></li></a>
+                  <a className="twitter"><li><img src="http://placehold.it/30x30" alt="Twitter" /></li></a>
+                  <a className="linkedin"><li><img src="http://placehold.it/30x30" alt="LinkedIn" /></li></a>
+                </nav>
+              </div>
+            </div>
           </div>
         </div>
       </div>
