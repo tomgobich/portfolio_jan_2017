8ae13940e61409bfa0bf7a794b27432ac1423039
Tom Gobich <fallofmars@gmail.com>
2016-12-18 15:40:16 -0500

    altered fullpageJS options

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,17 @@ class App extends Component {
 	render() {
 
 		let options = {
-			activeClass:          	'active', // the class that is appended to the sections links
-			anchors:              	['home', 'about'], // the anchors for each sections
-			arrowNavigation:      	true, // use arrow keys
-			className:            	'SectionContainer', // the class name for the section container
-			delay:                	1000, // the scroll animation speed
-			navigation:          	true, // use dots navigatio
-			navigationTooltips: 		['Home', 'About'],
-        	showActiveTooltip: 		true,
-			scrollBar:            	false, // use the browser default scrollbar
-			sectionClassName:     	'Section', // the section class name
-			sectionPaddingTop:    	'0', // the section top padding
-			sectionPaddingBottom: 	'0', // the section bottom padding
-			verticalAlign:        	false // align the content of each section vertical
+			activeClass:          	'active',
+			anchors:              	['home', 'about'],
+			arrowNavigation:      	true,
+			className:            	'SectionContainer',
+			delay:                	1000,
+			navigation:          	true,
+			scrollBar:            	false,
+			sectionClassName:     	'Section',
+			sectionPaddingTop:    	'0',
+			sectionPaddingBottom: 	'0',
+			verticalAlign:        	false
 		};
 
 		return (
