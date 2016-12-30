3d025b29fab46ed896e882f91512c79c480415cf
Tom Gobich <tomgobich@localhost.localdomain>
2016-12-30 11:59:03 -0500

    working on contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import PortfolioItem from './PortfolioItem';
-import PortfolioContent from '../assets/portfolio-content.json';
+import AnimatedButton from './AnimatedButton';
 
 
 class Contact extends Component {
@@ -9,34 +8,48 @@ class Contact extends Component {
 			<div id="contact" className="contact">
 				<div className="container-fluid">
 					<div className="row">
-            <div className="col-xs-12 col-lg-10 col-lg-offset-1">
-              <h2>Get In Touch</h2>
+            <div className="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+              <h2>Contact</h2>
               <div className="header-underscore"></div>
-              <p>
-                Feel free to contact me below
-              </p>
-              <div className="row">
-                <div className="col-xs-12 col-md-6">
-                  <form id="contactForm">
+              <ul className="contact-list">
+                <li>
+                  <a href="mailto:tom@tomgobich.com">
+                    <span className="mif-mail"></span> tom@tomgobich.com
+                  </a>
+                </li>
+                <li>
+                  <a href="tel:513-375-4400">
+                    <span className="mif-phone"></span> 513-375-4400
+                  </a>
+                </li>
+              </ul>
+              <form id="contactForm" className="contact-form">
+                <div className="row contact-content">
+                  <div className="col-xs-12 col-md-6 email-me">
                     <div className="form-group">
-                      <label for="name" className="control-label">Name:</label>
+                      <label className="control-label">Name:</label>
                       <input type="text" className="form-control" name="name" placeholder="Jon Snow" />
                     </div>
                     <div className="form-group">
-                      <label for="email" className="control-label">Email:</label>
+                      <label className="control-label">Email:</label>
                       <input type="email" className="form-control" name="email" placeholder="youknownothin@jonsnow.com" />
                     </div>
                     <div className="form-group">
-                      <label for="subject" className="control-label">Subject:</label>
+                      <label className="control-label">Subject:</label>
                       <input type="text" className="form-control" name="subject" placeholder="You Know Nothin' Jon Snow" />
                     </div>
+                  </div>
+                  <div className="col-xs-12 col-md-6 email-me">
                     <div className="form-group">
-                      <label for="name" className="control-label">Name:</label>
-                      <textarea rows="4" className="form-control" name="message" placeholder="Ygritte: Is that a palace? Jon Snow: It's a windmill." />
+                      <label className="control-label">Name:</label>
+                      <textarea rows="9" className="form-control" name="message" placeholder="Ygritte: Is that a palace? Jon Snow: It's a windmill." />
                     </div>
-                  </form>
+                    <div className="button-group">
+                      <AnimatedButton idName="btnContactSubmit" classList="btn btn-primary" text="Send Email" />
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
 			 		</div>
 		 		</div>
