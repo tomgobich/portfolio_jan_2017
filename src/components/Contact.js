5cfc3bbde506311db82eae0ffab77af327b9119e
Tom Gobich <fallofmars@gmail.com>
2016-12-30 15:04:54 -0500

    working on contact form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import AnimatedButton from './AnimatedButton';
 
 
 class Contact extends Component {
+
+  // constructor() {
+	// 	super();
+  //
+	// 	this.addFish = this.addFish.bind(this);
+	// 	this.loadSamples = this.loadSamples.bind(this);
+	// }
+
+  createEmail(e) {
+    e.preventDefault();
+    this.validateEntry(this.name.value);
+  }
+
+  // Validates input for some form of entry > length of 2
+  validateEntry(field) {
+
+  }
+
+  submitForm(data) {
+    $.ajax({
+      url: "https://formspree.io/tom@tomgobich.com",
+      method: "POST",
+      data: $('#contactForm').serialize(),
+      dataType: "json"
+    });
+  }
+
   render() {
+
+    let hiddenStyle = { display: "none" }
+
 		return (
 			<div id="contact" className="contact">
 				<div className="container-fluid">
@@ -23,26 +54,42 @@ class Contact extends Component {
                   </a>
                 </li>
               </ul>
-              <form id="contactForm" className="contact-form">
+              <form ref={(input) => this.contactForm = input} id="contactForm" className="contact-form" onSubmit={(e) => this.createEmail(e)}>
+                <input type="hidden" name="_next" value="/thanks" />
+                <input type="text" name="_gotcha" style={hiddenStyle} />
                 <div className="row contact-content">
                   <div className="col-xs-12 col-md-6 email-me">
                     <div className="form-group">
                       <label className="control-label">Name:</label>
-                      <input type="text" className="form-control" name="name" placeholder="Jon Snow" />
+                      <input ref={(input) => this.name = input}
+                             type="text"
+                             className="form-control"
+                             name="name"
+                             placeholder="Jon Snow" />
                     </div>
                     <div className="form-group">
                       <label className="control-label">Email:</label>
-                      <input type="email" className="form-control" name="email" placeholder="youknownothin@jonsnow.com" />
+                      <input ref={(input) => this.email = input}
+                             type="email"
+                             className="form-control"
+                             name="email"
+                             placeholder="youknownothin@jonsnow.com" />
                     </div>
                     <div className="form-group">
                       <label className="control-label">Subject:</label>
-                      <input type="text" className="form-control" name="subject" placeholder="You Know Nothin' Jon Snow" />
+                      <input ref={(input) => this.subject = input}
+                             type="text" className="form-control"
+                             name="_subject"
+                             placeholder="You Know Nothin' Jon Snow" />
                     </div>
                   </div>
                   <div className="col-xs-12 col-md-6 email-me">
                     <div className="form-group">
-                      <label className="control-label">Name:</label>
-                      <textarea rows="9" className="form-control" name="message" placeholder="Ygritte: Is that a palace? Jon Snow: It's a windmill." />
+                      <label className="control-label">Message:</label>
+                      <textarea ref={(input) => this.message = input}
+                                rows="9" className="form-control"
+                                name="message" 
+                                placeholder="Ygritte: Is that a palace? Jon Snow: It's a windmill." />
                     </div>
                     <div className="button-group">
                       <AnimatedButton idName="btnContactSubmit" classList="btn btn-primary" text="Send Email" />
